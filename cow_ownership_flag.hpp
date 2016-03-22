0c14877e3c5b024d9cf35a641335b820637d267e
Hadrien Grasland <grasland@lal.in2p3.fr>
2016-03-22 16:53:52 +0100

    Retour à la version mutex

--- cow_ownership_flag.hpp
+++ cow_ownership_flag.hpp
@@ -1,8 +1,6 @@
 #ifndef COW_OWNERSHIP_FLAG_H
 #define COW_OWNERSHIP_FLAG_H
 
-#include <atomic>
-#include <cstdint>
 #include <mutex>
 
 // Writing to copy-on-write data relies on an underlying notion of data ownership:
@@ -29,24 +27,20 @@ class cow_ownership_flag {
    
       // Ownership flags may be initialized to a certain value without synchronization, as at construction time only
       // one thread has access to the active ownership flag.
-      cow_ownership_flag(bool initially_owned) : m_ownership_status{to_ownership_status(initially_owned)} { }
-      
+      cow_ownership_flag(bool initially_owned) : m_owned{initially_owned} { }
       
       // When we move-construct from an ownership flag rvalue, we may assume that no other thread has access to either
       // that rvalue or the active flag, and avoid using synchronization.
-      cow_ownership_flag(cow_ownership_flag && other) : m_ownership_status{other.unsynchronized_status()} { }
-      
+      cow_ownership_flag(cow_ownership_flag && other) : m_owned{other.m_owned} { }
       
       // There's nothing special about deleting an ownership flag.
       ~cow_ownership_flag() = default;
       
-      
       // When we move-assign an ownership flag rvalue, no other thread has access to that rvalue, so we can access it
       // without synchronizing. But the active flag may be shared with other threads, so we need write synchronization.
       cow_ownership_flag & operator=(cow_ownership_flag && other) {
-         set_ownership_status(other.unsynchronized_status());
+         set_ownership(other.m_owned);
       }
-      
       
       // Ownership flags are not copyable. Proper CoW semantics would require clearing them upon copy,
       // which is at odds with normal copy semantics. It's better to throw a compiler error in this case,
@@ -54,67 +48,26 @@ class cow_ownership_flag {
       cow_ownership_flag(const cow_ownership_flag &) = delete;
       cow_ownership_flag & operator=(const cow_ownership_flag &) = delete;
       
-      
       // Authoritatively mark the active memory block as owned/not owned by the active thread
       void set_ownership(bool owned) {
-         set_ownership_status(to_ownership_status(owned));
+         std::lock_guard<std::mutex> lock(m_ownership_mutex);
+         m_owned = owned;
       }
-   
    
       // Acquire ownership of the active memory block once, using the provided resource acquisition routine,
       // if that's not done already. Have other threads block during this process.
-      void acquire_ownership_once(std::function<void()> acquisition_routine) {
-         // Try to switch the ownership status from NotOwner to AcquiringOwnership, and tell previous status
-         OwnershipStatusType previous_ownership = NotOwner;
-         m_ownership_status.compare_exchange_strong(previous_ownership,
-                                                    AcquiringOwnership,
-                                                    std::memory_order_acq_rel,
-                                                    std::memory_order_consume);
-         
-         // Act according to the previous ownership status
-         switch(previous_ownership) {
-            case NotOwner:  // Acquire resource ownership
-               acquisition_routine();
-               m_ownership_status.store(Owner,
-                                        std::memory_order_acq_rel);
-               break;
-               
-            case AcquiringOwnership:  // Wait for ownership acquisition
-               while(m_ownership_status.load(std::memory_order_consume) != Owner);
-               break;
-               
-            case Owner:  // Nothing to do, we already own the resource
-               break;
+      void acquire_ownership_once(std::function<void()> acquire) {
+         std::lock_guard<std::mutex> lock(m_ownership_mutex);
+         if(!m_owned) {
+            acquire();
+            m_owned = true;
          }
       }
       
-      
    private:
    
-      using OwnershipStatusType = std::uint_fast8_t;
-      enum OwnershipStatus : OwnershipStatusType { NotOwner, AcquiringOwnership, Owner };
-      std::atomic<OwnershipStatusType> m_ownership_status;
-      
-      static OwnershipStatusType to_ownership_status(bool is_owned) { return (is_owned ? Owner : NotOwner); }
-      
-      OwnershipStatusType unsynchronized_status() { return m_ownership_status.load(std::memory_order_relaxed); }
-      
-      void set_ownership_status(const OwnershipStatusType desired_ownership) {
-         OwnershipStatusType current_ownership = m_ownership_status.load(std::memory_order_consume);
-         
-         do {
-            // If we are currently acquiring resource ownership, wait for that operation to complete
-            while(current_ownership == AcquiringOwnership) {
-               current_ownership = m_ownership_status.load(std::memory_order_consume);
-            }
-         
-            // Once that is done, try to swap in the new resource ownership status
-         } while(m_ownership_status.compare_exchange_weak(current_ownership,
-                                                          desired_ownership,
-                                                          std::memory_order_acq_rel,
-                                                          std::memory_order_consume));
-      }
-      
+      std::mutex m_ownership_mutex;
+      bool m_owned;
 };
 
 #endif
