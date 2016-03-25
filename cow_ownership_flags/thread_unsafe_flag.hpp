371c6b60137b1b44ce87146ac3024685f7f8eb57
Hadrien Grasland <grasland@lal.in2p3.fr>
2016-03-25 18:40:14 +0100

    Added a license for distribution purposes

--- cow_ownership_flags/thread_unsafe_flag.hpp
+++ cow_ownership_flags/thread_unsafe_flag.hpp
@@ -1,3 +1,18 @@
+/*  This file is part of copy_on_write_ptr.
+
+    copy_on_write_ptr is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    copy_on_write_ptr is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with copy_on_write_ptr.  If not, see <http://www.gnu.org/licenses/>. */
+
 #ifndef THREAD_UNSAFE_FLAG_H
 #define THREAD_UNSAFE_FLAG_H
 
